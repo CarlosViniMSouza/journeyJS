8076147588a958ae1c8d9a0a3669469bde16a31d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-15 18:15:50 -0300

    Next Step: JS String Templates

--- code/script.js
+++ code/script.js
@@ -91,3 +91,18 @@ var newText = textExample.replace("will be changed", "was changed!");
 console.log(newText);
 
 // Link to JS String Methods: https://www.w3schools.com/js/js_string_methods.asp
+
+// JS Search Methods
+
+console.log("Position (indexOf): " + newText.indexOf("changed"));
+
+console.log("Position (lastIndexOf): " + newText.lastIndexOf("changed!"));
+
+// -1 = word not found
+console.log("Position (final): " + newText.lastIndexOf("kind"));
+
+console.log("Search: " + newText.search("was"));
+
+console.log("Search Result (boolean): " + newText.includes("text"));
+
+// Link to JS Search Methods: https://www.w3schools.com/js/js_string_search.asp
