aad1da037d7adfcc1eb44b6bcb22c6a3ad3504c2
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-15 17:44:15 -0300

    Next Step: JS String Search

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git init
 git add -A 
-git commit -m "Next Step: JS String Methods"
+git commit -m "Next Step: JS String Search"
 git push -u origin main
 git status
--- code/script.js
+++ code/script.js
@@ -53,7 +53,7 @@ imc = objPeople.IMC();
 
 console.log(objPeople);
 
-console.log(`The IMC Result is = ${imc}`);
+console.log(`\nThe IMC Result is = ${imc}`);
 
 // Strings (manipulate texts)
 
@@ -61,4 +61,33 @@ var firstName = "Carlos";
 var lastName = "Souza";
 var completeName = firstName + " " + lastName;
 
-console.log(completeName);
+console.log("\n" + completeName);
+
+// JS String Methods
+
+var textExample = "test example";
+
+console.log("\n" + `Size of text: ${textExample.length}`);
+
+// from 0 to 6 (with space, on case)
+console.log(textExample.slice(0, 6));
+
+// from 6 onwards
+console.log(textExample.slice(5));
+
+// replace() can be util:
+textExample = "\nThis text will be changed!";
+
+console.log(textExample);
+
+// replace() is case-sensitive:
+
+// 1. dont work:
+var newText = textExample.replace("Will Be Changed", "was changed!");
+
+// 2. Work:
+var newText = textExample.replace("will be changed", "was changed!");
+
+console.log(newText);
+
+// Link to JS String Methods: https://www.w3schools.com/js/js_string_methods.asp
