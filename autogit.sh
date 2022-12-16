1de68605f25d4e0a94a6f015b10a5da22492b808
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-16 20:33:51 -0300

    Next Step: JS Arrays

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git init
 git add -A 
-git commit -m "Next Step: JS Number Properties"
+git commit -m "Next Step: JS Arrays"
 git push -u origin main
 git status
--- src/script.js
+++ src/script.js
@@ -139,3 +139,21 @@ console.log(`${Number.isInteger(numTest01)}`);
 console.log(`${Number.parseInt(numTest01)}`);
 
 // Link to Number Methods: https://www.w3schools.com/js/js_number_methods.asp
+
+var num = Number.EPSILON;
+
+console.log(`\nThe Epsilon: ${num}`);
+
+// OBS.: Number Properties CANNOT BE used on Variables
+
+var numPositInf = Number.POSITIVE_INFINITY;
+
+console.log(`The positive infinity: ${numPositInf}`);
+
+var numNegatInf = Number.NEGATIVE_INFINITY;
+
+console.log(`The negative infinity: ${numNegatInf}`);
+
+// I dont see reasons for use this number properties ... 👀
+
+// Link to Number Properties: https://www.w3schools.com/js/js_number_properties.asp
