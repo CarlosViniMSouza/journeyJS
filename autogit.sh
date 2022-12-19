562b7ae4d57d895f933c4423ad67fdfd9e3808ab
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-19 11:19:34 -0300

    Next Step: JavaScript Array reduceRight()

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git init
 git add -A 
-git commit -m "Next Step: JavaScript Array filter()"
+git commit -m "Next Step: JavaScript Array reduceRight()"
 git push -u origin main
 git status
 
--- useCases/case03Arrays/case10ArrayIteration.js
+++ useCases/case03Arrays/case10ArrayIteration.js
@@ -23,3 +23,27 @@ function mapFunction(values) {
 }
 
 console.log(numsArray02);
+
+// 3. filter() -> create a new array with array elements that pass a test. 
+// (similar to map() method)
+
+var numsArray01 = [10, 20, 30, 40, 50];
+var numsArray02 = numsArray01.filter(filterFunction);
+
+function filterFunction(values) {
+    return values >= 25;
+}
+
+console.log(numsArray02);
+
+// 4. reduce() -> runs a function on each array element to produce a single value.
+// (The reduce() method does not reduce the original array.)
+
+var arrayNums = [1, 10, 5, 7, 3, 8];
+var numsReduce = arrayNums.reduce(reduceFunction);
+
+function reduceFunction(values, total) {
+    return values + total;
+}
+
+console.log(numsReduce);
