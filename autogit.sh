0f69de91b45da2f6732f0b2cb7035504301b87e0
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-21 12:08:05 -0300

    Next Step: JS Date Formats

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,7 @@
 git init
 git add -A 
-git commit -m "Exercises -> Question 13 done"
+git commit -m "Next Step: JS Date Formats"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_dates.asp
+# link: https://www.w3schools.com/js/js_date_formats.asp
---
+++ useCases/case04Dates/case11DateIntro.js
@@ -0,0 +1,9 @@
+// 1. new Date() -> creates a date object with the current date and time:
+
+const dateNow = new Date();
+console.log(dateNow);
+
+// .2 new Date(date string) -> creates a date object from a date string:
+
+var dateCurrent = new Date("2022-12-21");
+console.log("\n" + dateCurrent);
