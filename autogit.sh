da0273a77d067cba8e02f35e5887e3a4e3a76d67
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-21 20:55:04 -0300

    Next Step: JS Random

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,7 @@
 git init
 git add -A 
-git commit -m "Next Step: JS Math"
+git commit -m "Next Step: JS Random"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_math.asp
+# link: https://www.w3schools.com/js/js_random.asp
---
+++ useCases/case01Operations/case12Math.js
@@ -0,0 +1,11 @@
+console.log(Math.round(4.5));
+
+console.log(Math.ceil(4.5));
+
+console.log(Math.floor(4.5));
+
+console.log(Math.trunc(4.5));
+
+// I tired.
+
+// link to more methods and properties of Math: https://www.w3schools.com/js/js_math.asp
