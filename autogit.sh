874941540f6005f3556520d85d77d7adebe8c6c4
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-04 21:38:19 -0300

    Next Step: JS For Loop (part 2)

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: JS For Loop"
+git commit -m "Next Step: JS For Loop (part 2)"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_loop_for.asp
+# link: https://www.w3schools.com/js/js_loop_forof.asp
