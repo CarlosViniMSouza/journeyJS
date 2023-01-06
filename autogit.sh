aea6e1aa61380d4aae76a2891ea877a872555e6d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-06 17:20:19 -0300

    Next Step: JS For While

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: JS For Loop (part 2)"
+git commit -m "Next Step: JS For While"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_loop_forof.asp
+# link: https://www.w3schools.com/js/js_loop_while.asp
---
+++ useCases/case05Loops/case19LoopOf.js
@@ -0,0 +1,9 @@
+const cars = ['\ncorolla', '\nford', '\nvowks'];
+
+var txtCars = '';
+
+for (let i of cars) {
+    txtCars += i;
+}
+
+console.log(txtCars);
