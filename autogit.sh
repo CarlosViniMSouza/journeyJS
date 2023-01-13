94791dd62b2d1f07d777ccafef9d6cc0b8b6c91b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-12 22:21:03 -0300

    Next Step: JS Bitwise Operations

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: JS Type Conversion"
+git commit -m "Next Step: JS Bitwise Operations"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_type_conversion.asp
+# link: https://www.w3schools.com/js/js_bitwise.asp
--- useCases/case06Others/case23Types.js
+++ useCases/case06Others/case23Types.js
@@ -32,3 +32,31 @@ console.log((15.67).constructor);
 console.log([1, "Array: ", true].constructor);
 
 // for more infos, access: https://www.w3schools.com/js/js_typeof.asp
+
+/* JS variables can be converted to a new variable and another data type: */
+
+// 1. Convert String to Number:
+
+const numberTxt = "10025";
+
+console.log("\n" + typeof Number(numberTxt));
+
+// 2. Convert Numbers to Strings:
+
+const textNum = "12345";
+
+console.log(typeof textNum.toString());
+
+// 3. Converting Dates to Numbers: 👀 ~ !!
+
+const newDate = new Date("2023-12-01");
+
+console.log(typeof Number(newDate));
+
+console.log(`\nConverted to number: ${Number(newDate)}`);
+
+// 4. Convert Dates to Strings:
+
+console.log(`Converted to string: ${String(newDate)}`); // toString() works too!
+
+// for more infos, access: https://www.w3schools.com/js/js_type_conversion.asp
