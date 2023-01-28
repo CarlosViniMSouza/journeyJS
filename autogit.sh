7d08672b490ed2780eba3953b6f2aa0f7c7fd304
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-28 15:17:26 -0300

    Next Step: JS this Keyword

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: JS Use Strict"
+git commit -m "Next Step: JS this Keyword"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_strict.asp
+# link: https://www.w3schools.com/js/js_this.asp
