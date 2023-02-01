624ea8cb3814c8cf98387d5241e501f2bc2ae3f9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-02-01 13:31:41 -0300

    Next Step: JS Objects Properties

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: JS Debugging"
+git commit -m "Next Step: JS Objects Properties"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_debugging.asp
+# link: https://www.w3schools.com/js/js_object_properties.asp
