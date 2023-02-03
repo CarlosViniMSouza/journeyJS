59f32da3db4e2f55ed9613ecaa96f7133096fa3f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-02-03 15:32:18 -0300

    Next Step: JS Functions Definitions

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: JS Object Prototypes"
+git commit -m "Next Step: JS Functions Definitions"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_object_prototypes.asp
+# link: https://www.w3schools.com/js/js_function_definition.asp
--- useCases/case03Arrays/case22Maps.js
+++ useCases/case03Arrays/case22Maps.js
@@ -28,3 +28,23 @@ console.log(newMap02.get("key2"));
 console.log("\nThe map 01 size is: " + newMap01.size);
 
 // For more infos, access: https://www.w3schools.com/js/js_maps.asp
+
+// Objects Sets (https://www.w3schools.com/js/js_object_sets.asp)
+
+const mySet = new Set(["string", 12, 15.67, true, ["more a array", null]]);
+
+mySet.forEach(readProps);
+
+function readProps(data) {
+    console.log(data);
+}
+
+console.log(mySet.values());
+
+/*
+    'keys()' returns the same as values().
+
+    This makes Sets compatible with Maps.
+*/
+
+console.log(mySet.keys());
