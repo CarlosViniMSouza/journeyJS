a4245c48df733bfe0e5e8df7cbd923f98ffcd509
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-02-05 18:44:46 -0300

    NodeJS File System -> Read Files

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next Step: NodeJS HTTP Module"
+git commit -m "NodeJS File System -> Read Files"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/nodejs/nodejs_http.asp
+# link: https://www.w3schools.com/nodejs/nodejs_filesystem.asp
