6b376cc7134f90a974b95bd69581d7d416c08014
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-25 16:18:54 -0300

    JS Classes

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "JS Closures"
+git commit -m "JS Classes"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_function_closures.asp
+# link: https://www.w3schools.com/js/js_class_intro.asp
