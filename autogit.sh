f781ef5ae277e088032a096c9cc44bd296c3d1a5
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-25 13:21:42 -0300

    JS Function apply()

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "JS Function Invocation"
+git commit -m "JS Function apply()"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_function_invocation.asp
+# link: https://www.w3schools.com/js/js_function_apply.asp
