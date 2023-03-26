e0e892316cd49a680c1cd1234e14f9ee3137ad09
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-26 10:34:41 -0300

    JS Callbacks

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "JS Static Methods"
+git commit -m "JS Callbacks"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_class_static.asp
+# link: https://www.w3schools.com/js/js_callback.asp
