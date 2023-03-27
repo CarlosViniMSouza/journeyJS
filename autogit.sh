dbdf56ce834ed90347160e6c158e8695158deaf1
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-26 22:14:12 -0300

    JS HTML DOM

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "JS Async"
+git commit -m "JS HTML DOM"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_async.asp
+# link: https://www.w3schools.com/js/js_htmldom.asp
