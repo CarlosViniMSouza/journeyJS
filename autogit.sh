a3a1c704fb64e83fecc50081f6899bd77ee3eca9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-04-01 22:09:57 -0300

    JS DOM - Changing HTML

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "JS DOM Elements"
+git commit -m "JS DOM - Changing HTML"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_htmldom_elements.asp
+# link: https://www.w3schools.com/js/js_htmldom_html.asp
