a72a36832e0e41777451b12d1e5d96592e1e5f48
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-04-03 18:58:59 -0300

    JS DOM - Forms

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "JS DOM - Changing HTML"
+git commit -m "JS DOM - Forms"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_htmldom_html.asp
+# link: https://www.w3schools.com/js/js_validation.asp
