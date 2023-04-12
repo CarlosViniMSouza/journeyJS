bfe4b2a298cf5893e3fd49b4037727996dbfdbc2
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-04-12 18:21:16 -0300

    markdown folder -> finished | DOM study -> started

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "markdown folder -> useCase08-Asynchronous.md finished"
+git commit -m "markdown folder -> finished | DOM study -> started"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_validation.asp
+# link: https://www.w3schools.com/js/js_htmldom_events.asp
---
+++ src/script.js
@@ -0,0 +1,9 @@
+const nameUser = "CarlosViniMSouza";
+
+try {
+    if (nameUser === null) {
+        return 'Name is Null';
+    }
+} catch (error) {
+    return 'Whitout name field';
+}
