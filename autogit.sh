7348cd77e217240a2cf94b00364433eb52d64c4d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-04-16 22:40:53 -0300

    Next: Web History API

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Improved: markdown/tutorial-basic"
+git commit -m "Next: Web History API"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_validation_api.asp
+# link: https://www.w3schools.com/js/js_api_history.asp
--- src/tutorial-web/script02-Web-API-Forms.js
+++ src/tutorial-web/script02-Web-API-Forms.js
@@ -1,8 +1,26 @@
-function myFunction() {
+function myFunctionNum01() {
     const value = document.getElementById("id-01");
 
     if (!value.checkValidity()) {
         document.getElementById("demo").innerHTML = value.validationMessage;
         // if true -> return 'Please fill out this field.'
+    }
+}
+
+function myFunctionNum02() {
+    let insertText = "value";
+
+    if (document.getElementById("id-02").validity.rangeOverflow) {
+        insertText = "Value too large";
+    }
+
+    // This doesn't return the expected message!! 
+}
+
+function myFunctionNum03() {
+    let myText = "value";
+
+    if (document.getElementById("id-03").validity.rangeUnderflow) {
+        myText = "Value too small";
     }
 }
