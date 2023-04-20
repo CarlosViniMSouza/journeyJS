d2ef470c40e9616030f6464efc5196d3512ecb1f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-04-20 13:54:05 -0300

    Next: Web Workers API

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Next: Web Storage API"
+git commit -m "Next: Web Workers API"
 git push -u origin main
 git status
 
-# link: https://www.w3schools.com/js/js_api_web_storage.asp
+# link: https://www.w3schools.com/js/js_api_web_workers.asp
